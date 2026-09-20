d591ec4d13c65938d99c985045341c4681f515b3
kesaribyte <shitoleomraje12@gmail.com>
2026-09-20 18:25:05 +0530

    add app componet

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    <h1>chai aur react {count}</h1>
+    <p>test para</p>
+   </>
+  )
+}
+
+export default App
